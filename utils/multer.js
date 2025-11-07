e9202861d11f9c0e115277debdbce59a344a0e35
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2025-11-07 09:12:38 +0530

    upload 6

--- utils/multer.js
+++ utils/multer.js
@@ -19,6 +19,8 @@ const storage = multer.diskStorage({
   }
 });
 
+
+
 // File type filter (optional)
 const fileFilter = (req, file, cb) => {
   const allowedTypes = /jpeg|jpg|png|pdf|mp4|mp3|wav|avi|mkv|webm/;
